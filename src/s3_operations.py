d0649ca2ea20b84a97a9049e5544be93747e2da5
Andrew <andrewozo123@gmail.com>
2024-10-23 20:11:20 -0400

    uploaded files to S3

--- src/s3_operations.py
+++ src/s3_operations.py
@@ -1,1 +1,15 @@
 from auth import auth
+
+def upload_file(object_key, filename):
+    s3_resource, s3_bucket = auth("../config.ini") 
+
+    try:
+        bucket = s3_resource.Bucket(s3_bucket)
+        bucket.upload_file(f'../{filename}',object_key)
+        return True
+    except Exception as e:
+        print(f'Error uploading file to s3: {str(e)}')
+        return False
+    
+
+upload_file("projectdata.csv","projectdata.csv")
